9f8db259e5d52f493d3e6b84b23899ecf8bb52c2
Nick Tonkin <1nickt@users.noreply.github.com>
2017-11-25 18:11:36 -0500

    Use send_as built-in to return JSON. (Also fix some whitespace inconsistencies.)

--- lib/ajtest.pm
+++ lib/ajtest.pm
@@ -3,42 +3,38 @@ use Dancer2;
 use LWP::UserAgent;
 use Net::Whois::Raw;
 
-our $VERSION = '0.1';
+our $VERSION = '0.11';
 
 get '/' => sub {
     template 'index' => { 'title' => 'ajtest' };
 };
 
 get '/aj/whois' => sub {
-	header 'Content-Type' => 'application/json';
-	#my @ret =  whois('perl.org');
-	my $dominfo = whois('perl.org');
-	my $ret;
-	foreach my $it(split /\n/, $dominfo){
-		$ret=$1 if $it=~/Name Server: (.*)$/;
-	}
-    return to_json { text => "$ret" };
+    my $dominfo = whois('perl.org');
+    my $ret;
+    foreach my $it(split /\n/, $dominfo){
+        $ret=$1 if $it=~/Name Server: (.*)$/;
+    }
+    send_as JSON => { text => $ret };
 };
 
 
 get '/aj/ping' => sub {
-     header 'Content-Type' => 'application/json';
-	 open my $cmd,'ping -n 4 www.perl.org|';
-	 my $ret;
-	 while (<$cmd>){
-		if($_=~/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/){
-			$ret = $1;
-		
-		}
-	 }
-	 return to_json { text => "$ret" };
+     open my $cmd,'ping -n 4 www.perl.org|';
+     my $ret;
+     while (<$cmd>){
+        if($_=~/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/){
+            $ret = $1;
+        
+        }
+     }
+     send_as JSON => { text => "$ret" };
 };
 
 get '/aj/lwp' => sub {
-	header 'Content-Type' => 'application/json';
     my $ua = LWP::UserAgent->new;
     my $r = $ua->get('http://www.perl.org/');
-	return to_json { text => $r->code." ".$r->message };
+    send_as JSON => { text => $r->code." ".$r->message };
 };
 
 true;
